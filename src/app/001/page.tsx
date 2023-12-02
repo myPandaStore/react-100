144e0d9486835403cd6c0b7b0a4d0ad688aac81b
luckin <1832114807@qq.com>
2023-12-02 17:04:39 +0800

    chore: init commit

---
+++ src/app/001/page.tsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: luckin 1832114807@qq.com
+ * @Date: 2023-12-02 16:56:08
+ * @LastEditors: luckin 1832114807@qq.com
+ * @LastEditTime: 2023-12-02 17:01:00
+ * @FilePath: \my-100\src\app\001\page.tsx
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+export default function Page() {
+    return <h1>Hello, 001 Page!</h1>
+}
