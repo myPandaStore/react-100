4416c4eadb54f9196afa68446af101b9330d52da
luckin <1832114807@qq.com>
2024-01-15 12:07:32 +0800

    chore: finish ship

--- src/app/011/page.tsx
+++ src/app/011/page.tsx
@@ -2,28 +2,69 @@
  * @Author: luckin 1832114807@qq.com
  * @Date: 2024-01-14 20:02:12
  * @LastEditors: luckin 1832114807@qq.com
- * @LastEditTime: 2024-01-15 10:46:20
+ * @LastEditTime: 2024-01-15 12:05:21
  * @FilePath: \react-100\src\app\011\page.tsx
  * @Description: 
  * 
  * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
  */
 'use client'
+import { useSearchParams } from 'next/navigation'
 import { useDeviceOrientation } from '../Hooks'
+import Paper from '../components/paper'
+import Note from '../components/note'
 
-
+function clamp(n: number, min: number, max: number) {
+    if (n < min)
+        return min
+    if (n > max)
+        return max
+    return n
+}
 export default function Ship() {
-    const orientation = useDeviceOrientation()
-    // console.log('orientation is:', orientation)
+    const { alpha, beta, gamma } = useDeviceOrientation()
+    const searchParams = useSearchParams()
+    const shot = searchParams.get('shot')
+    const debug = searchParams.get('debug')
 
-    const theta = Math.random() * 2 * Math.PI
-    const transformStyle = `rotate(${theta}rad)`
+    function getTheta() {
+        if (shot) {
+            return Math.PI / 2 * 4.55
+        }
+        if (beta === null || gamma === null) {
+            return 0
+        }
+        const x = clamp(beta, -90, 90) / 90
+        const y = clamp(-gamma, -90, 90) / 90
+        return Math.atan2(y, x)
+    }
+
+    const transformStyle = `rotate(${getTheta()}rad)`
+    const debugContent = debug
+        ? <div >
+            <p>Gamma {gamma}</p>
+            <p>Beta {beta}</p>
+            <p>Theta {getTheta()}</p>
+            <p>Alhpa {alpha}</p>
+        </div>
+        : null
 
     return (
-        <div className="centered" style={{ transform: transformStyle }}>
-            <p>A ship in harbor is safe,<br />but that is not what ships <br />are built for.</p>
-            <br />
-            <em>- John A. Shedd</em>
-        </div>
+        <>
+            <Paper>
+                <div className="box centered rounded-full text-center flex flex-col justify-center">
+                    <div style={{ transform: transformStyle }}>
+
+                        <p>A ship in harbor is safe,<br />but that is not what ships <br />are built for.</p>
+                        <br />
+                        <em>- John A. Shedd</em>
+                    </div>
+
+                </div>
+            </Paper>
+            <Note>
+                {debugContent}
+            </Note>
+        </>
     )
 }
--- src/app/Hooks/useDeviceOrientation.tsx
+++ src/app/Hooks/useDeviceOrientation.tsx
@@ -2,14 +2,13 @@
  * @Author: luckin 1832114807@qq.com
  * @Date: 2024-01-15 10:24:21
  * @LastEditors: luckin 1832114807@qq.com
- * @LastEditTime: 2024-01-15 10:51:54
+ * @LastEditTime: 2024-01-15 11:02:50
  * @FilePath: \react-100\src\app\Hooks\useDeviceOrientation.tsx
  * @Description: 
  * 
  * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
  */
-import { useEventListener } from "ahooks";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const isClient = typeof window !== "undefined";
 const defaultWindow = isClient ? window : void 0;
@@ -24,18 +23,19 @@ export default function useDeviceOrientation(options: ConfigurableWindow = {}) {
     const [alpha, setAlpha] = useState<OrientationType>(null);
     const [beta, setBeta] = useState<OrientationType>(null);
     const [gamma, setGama] = useState<OrientationType>(null);
-    // if (window && isSupported) {
-    useEventListener("deviceorientation", (event) => {
-        setIsAbsolute(event.absolute);
-        setAlpha(event.alpha);
-        setBeta(event.beta);
-        setGama(event.gamma);
-    });
-    // }
-    // console.log(!!window.DeviceOrientationEvent)
-    console.log(alpha, beta, gamma)
-    const testmessage: string = 'alpha：' + alpha + '  beta：' + beta + '  gama：' + gamma
-    alert(testmessage)
+
+    useEffect(() => {
+        if (isSupported && window) {
+            window.addEventListener("deviceorientation", (event) => {
+                setIsAbsolute(event.absolute);
+                setAlpha(event.alpha);
+                setBeta(event.beta);
+                setGama(event.gamma);
+            });
+        }
+    }, [isSupported, window])
+
+
     return {
         isSupported,
         isAbsolute,
