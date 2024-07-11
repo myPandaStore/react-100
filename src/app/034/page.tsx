5d9ecbab3c97b4be945afe8d60f2719783b1cb8e
luckin <yipeng.zhang@kunlun-inc.com>
2024-07-11 22:16:55 +0800

    feat: update 034 page with changes

--- src/app/034/page.tsx
+++ src/app/034/page.tsx
@@ -2,7 +2,7 @@
  * @Author: luckin yipeng.zhang@kunlun-inc.com
  * @Date: 2024-07-10 22:39:48
  * @LastEditors: luckin yipeng.zhang@kunlun-inc.com
- * @LastEditTime: 2024-07-10 22:50:20
+ * @LastEditTime: 2024-07-11 22:15:30
  * @FilePath: /react-100/src/app/034/page.tsx
  * @Description: 
  * 
@@ -12,9 +12,10 @@
 import { useEffect, useRef } from "react"
 import { p5i } from 'p5i'
 import type { P5I } from 'p5i'
+import Paper from '../components/paper'
 
 export default function Page() {
-    const el = useRef<HTMLCanvasElement | null>(null)
+    const el = useRef<any>(null)
 
     const { random, trunc, min } = Math
     const {
@@ -35,15 +36,15 @@ export default function Page() {
     const SPACING = 10
     const AMOUNT = 2000
 
-    // function getForceOnPoint(x: number, y: number, z: number) {
-    //     // https://p5js.org/reference/#/p5/noise
-    //     return (noise(x / SCALE, y / SCALE, z / SCALE_Z) - 0.5) * 2 * TWO_PI
-    // }
+    function getForceOnPoint(x: number, y: number, z: number) {
+        // https://p5js.org/reference/#/p5/noise
+        return (noise(x / SCALE, y / SCALE, z / SCALE_Z) - 0.5) * 2 * TWO_PI
+    }
 
     let points: { x: number; y: number; t: number }[] = []
 
     function setup() {
-        createCanvas(400, 400)
+        createCanvas(398, 398)
         background('#fff')
         stroke('#888')
         noFill()
@@ -55,51 +56,56 @@ export default function Page() {
             points.push({ x: random() * w, y: random() * h, t: 0 })
     }
 
-    // function draw({ frameCount, mouseX, mouseY }: P5I) {
-    //     background(255, 255, 255, 50)
+    function draw({ frameCount, mouseX, mouseY }: P5I) {
+        background(255, 255, 255, 50)
 
-    //     points = points
-    //         .filter(({ x, y }) => x > 0 && x < w && y > 0 && y < h)
+        points = points
+            .filter(({ x, y }) => x > 0 && x < w && y > 0 && y < h)
 
-    //     const len = points.length
-    //     const n = AMOUNT - points.length
+        const len = points.length
+        const n = AMOUNT - points.length
 
-    //     if (n > 0) {
-    //         for (let i = 0; i < n; i++) {
-    //             const { x, y } = points[trunc(random() * len)]
-    //             points.push({
-    //                 x: x + (random() - 0.5) * LENGTH * 50,
-    //                 y: y + (random() - 0.5) * LENGTH * 50,
-    //                 t: 0,
-    //             })
-    //         }
-    //     }
+        if (n > 0) {
+            for (let i = 0; i < n; i++) {
+                const { x, y } = points[trunc(random() * len)]
+                points.push({
+                    x: x + (random() - 0.5) * LENGTH * 50,
+                    y: y + (random() - 0.5) * LENGTH * 50,
+                    t: 0,
+                })
+            }
+        }
 
-    //     for (const p of points) {
-    //         const { x, y, t } = p
-    //         stroke(0, 0, 0, min(t * 10, 200))
-    //         const rad = getForceOnPoint(x, y, mouseX + mouseY + frameCount / 2)
-    //         const nx = x + cos(rad) * LENGTH
-    //         const ny = y + sin(rad) * LENGTH
-    //         line(x, y, nx, ny)
-    //         p.x = nx
-    //         p.y = ny
-    //         p.t += 1
-    //     }
-    // }
+        for (const p of points) {
+            const { x, y, t } = p
+            stroke(0, 0, 0, min(t * 10, 200))
+            const rad = getForceOnPoint(x, y, mouseX + mouseY + frameCount / 2)
+            const nx = x + cos(rad) * LENGTH
+            const ny = y + sin(rad) * LENGTH
+            line(x, y, nx, ny)
+            p.x = nx
+            p.y = ny
+            p.t += 1
+        }
+    }
 
-    // function restart() {
-    //     console.log('restart')
-    //     if (el.current)
-    //         mount(el.current, { setup, draw })
-    // }
+    function restart() {
+        if (el.current)
+            mount(el.current, { setup, draw })
+    }
 
     useEffect(() => {
-        // restart()
+        restart()
+
+        return () => {
+            unmount()
+        }
     }, [])
 
     return (
-        <div className="container" ref={el}>
-        </div>
+        <Paper>
+            <div className="container centered box" ref={el} onClick={() => restart()}>
+            </div>
+        </Paper>
     )
 }
